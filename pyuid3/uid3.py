Fixed statistics calculation over subsets of splitting data

pyuid3/uid3.py
@@ -65,15 +65,20 @@ class UId3(BaseEstimator):
             stats = data.calculate_statistics(a)
             
             ## start searching for best border values  -- such that class value remains the same for the ranges between them
-            border_search_list = []
-            for i in data.get_instances():
-                v=i.get_reading_for_attribute(a).get_most_probable().get_name()
-                border_search_list.append([v])
-            border_search_df = pd.DataFrame(border_search_list,columns=['values'])
-            border_search_df['class'] = cl
-            border_search_df=border_search_df.sort_values(by='values')
-            border_search_df['class_shitf'] = border_search_df['class'].shift(1)
-            values = border_search_df[border_search_df['class_shitf'] != border_search_df['class']]['values'].astype('str')
+            if a.get_type() == Attribute.TYPE_NUMERICAL:
+                border_search_list = []
+                for i in data.get_instances():
+                    v=i.get_reading_for_attribute(a).get_most_probable().get_name()
+                    border_search_list.append([v])
+                border_search_df = pd.DataFrame(border_search_list,columns=['values'])
+                border_search_df['values']=border_search_df['values'].astype('f8')
+                border_search_df['class'] = cl
+                border_search_df=border_search_df.sort_values(by='values')
+                border_search_df['class_shitf'] = border_search_df['class'].shift(1)
+                values_a = border_search_df[border_search_df['class_shitf'] != border_search_df['class']]['values'].astype('str')
+                border_search_df['class_shitf'] = border_search_df['class'].shift(-1)
+                values_b=border_search_df[border_search_df['class_shitf'] != border_search_df['class']]['values'].astype('str')
+                values = np.unique(np.concatenate((values_a,values_b)))
             ## stop searching for best border values
             for v in values:  
                 subdata = None
@@ -84,14 +89,15 @@ class UId3(BaseEstimator):
                 elif a.get_type() == Attribute.TYPE_NUMERICAL:
                     subdata_less_than,subdata_greater_equal = data.filter_numeric_attribute_value(a, v)
                 if a.get_type() == Attribute.TYPE_NOMINAL:
-                    temp_gain -= len(subdata)/len(data)*stats.get_stat_for_value(v) * entropyEvaluator.calculate_entropy(subdata) 
+                    temp_gain -= (stats.get_stat_for_value(v)) * entropyEvaluator.calculate_entropy(subdata) 
                 elif a.get_type() == Attribute.TYPE_NUMERICAL:
-                    single_temp_gain = entropy - stats.get_stat_for_value(v) * (len(subdata_less_than)/len(data)*entropyEvaluator.calculate_entropy(subdata_less_than) + len(subdata_greater_equal)/len(data)*entropyEvaluator.calculate_entropy(subdata_greater_equal))
+                    total_stat_for_lt_value = stats.get_total_stat_for_lt_value(v)
+                    single_temp_gain = entropy - (total_stat_for_lt_value/len(data)*entropyEvaluator.calculate_entropy(subdata_less_than) + (1-total_stat_for_lt_value)/len(data)*entropyEvaluator.calculate_entropy(subdata_greater_equal))
                     if single_temp_gain >= temp_numeric_gain:
                         temp_numeric_gain = single_temp_gain
                         temp_gain = single_temp_gain
-                        a.set_value_to_split_on(v) #UPS
-            if temp_gain > 0 and temp_gain >= info_gain:
+                        a.set_value_to_split_on(v) 
+            if temp_gain >= info_gain:
                 info_gain = temp_gain
                 best_split = a
                 a.set_importance_gain(info_gain)
@@ -103,7 +109,6 @@ class UId3(BaseEstimator):
             root.set_type(class_att.get_type())
             result = Tree(root)
             return result
-
         # Create root node, and recursively go deeper into the tree.
         class_att = data.get_class_attribute()
         class_stats = data.calculate_statistics(class_att)

src/pyuid3/att_stats.py
@@ -5,6 +5,7 @@ __all__ = ['AttStats']
 # Cell
 from typing import List,Dict
 import pandas as pd
+import numpy as np
 
 from .value import Value
 from .attribute import Attribute
@@ -12,9 +13,10 @@ from .attribute import Attribute
 
 # Cell
 class AttStats:
-    def __init__(self, statistics: Dict[str,Value], avg_confidence: float):
+    def __init__(self, statistics: Dict[str,Value], avg_confidence: float, att_type: int):
         self.statistics = statistics
         self.avg_confidence = avg_confidence
+        self.att_type = att_type
 
     @staticmethod
     def calculate_statistics(att: Attribute, data: 'Data') -> 'AttStats':    # TODO: rename to get_stats
@@ -22,7 +24,7 @@ class AttStats:
         avg_conf = 0
 
         if not data.get_instances():
-            return AttStats(conf_sum, avg_conf)
+            return AttStats(conf_sum, avg_conf, att.get_type())
 
         instances = data.get_instances()
         att_name=att.get_name()
@@ -42,23 +44,37 @@ class AttStats:
         avg_conf /= size
         stats = {}
         for stat_v in conf_sum.values():
-            stats[stat_v.get_name()]=(Value(stat_v.get_name(), stat_v.get_confidence()/size))
-        return AttStats(stats, avg_conf)
+            #Walkaround to deal with numerical values that can have decimal places, e.g.to make sure  3 == 3.0
+            if att.get_type() == Attribute.TYPE_NUMERICAL:
+                stats[str(float(stat_v.get_name()))]=(Value(stat_v.get_name(), stat_v.get_confidence()/size))
+            else:
+                stats[stat_v.get_name()]=(Value(stat_v.get_name(), stat_v.get_confidence()/size))
+        return AttStats(stats, avg_conf, att.get_type())
 
 
 
 
-    def get_statistics(self) -> List[Value]:   # TODO: rename to get_statistics
+    def get_statistics(self) -> List[Value]: 
         return list(self.statistics.values())
 
     def get_avg_confidence(self) -> float:
         return self.avg_confidence
 
     def get_stat_for_value(self, value_name: str) -> float:
+        #Walkaround in case of numerical values having decimal places, e.g.to make sure  3 == 3.0
+        if self.att_type == Attribute.TYPE_NUMERICAL:
+            value_name = str(float(value_name))
         if value_name in self.statistics.keys():
             return self.statistics[value_name].get_confidence()
         else:
             return 0
+        
+    def get_total_stat_for_lt_value(self, value_name: str) -> float:        
+        return np.sum([c.get_confidence() for v,c in self.statistics.items() if float(value_name) > float(v)])
+    
+    def get_total_stat_for_gte_value(self, value_name: str) -> float:        
+        return np.sum([c.get_confidence() for v,c in self.statistics.items() if float(value_name) <= float(v)])
+        
 
     def get_most_probable(self) -> Value:
         statistics = list(self.statistics.values())